5e776447b1bc7a35023490021492fb71ec1dd18f
Yaya Nur <yamyam5000@gmail.com>
2023-09-07 12:51:27 -0500

    fixed utils fixed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
+// Load the Mongoose ORM
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
